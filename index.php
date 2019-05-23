87d457532faabc40a846de1f47c5103332e7d3cb
Antonio Orlando <horlandomagic@gmail.com>
2019-05-23 20:41:27 -0300

    adicionada o metodo delete

--- index.php
+++ index.php
@@ -1,39 +1,53 @@
 <?php
 
 require_once("config.php");
+/*
+$sql = new Sql();
 
-//$sql = new Sql();
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+echo json_encode($usuarios);
+*/
 
-//echo json_encode($usuarios);
+/*
+$user = new Usuario();
 
-//$user = new Usuario();
+$user->loadById(3); //traz apenas os dados de uma entrada do banco de dados
 
-//$user->loadById(3); //traz apenas os dados de uma entrada do banco de dados
+echo $user;
+*/
 
-//echo $user;
+/*
+$lista = Usuario::getList();//como o método é estático não é necessário instânciar um novo objeto da classe para fazer uso dele
 
-//$lista = Usuario::getList();//como o método é estático não é necessário instânciar um novo objeto da classe para fazer uso dele
+echo json_encode($lista);
+*/
 
-//echo json_encode($lista);
+/*
+$busca = Usuario::search("toni");//busca por varias entradas que contenham o mesmo texto
 
-//$busca = Usuario::search("toni");//busca por varias entradas que contenham o mesmo texto
+echo json_encode($busca);
+*/
 
-//echo json_encode($busca);
+/*
+//validando um usuário
+$usuario = new Usuario();
 
-//$usuario = new Usuario();
+$usuario->login("Orland","mypassword");
 
-//$usuario->login("Orland","mypassword");
+echo $usuario;
+*/
 
-//echo $usuario;
+/*
+$aluno = new Usuario("Wagner", "CDDTuc");//foi criado o método construct que permite passar os dados direto pela instância do objeto sem precisar chamar os setters
 
-//$aluno = new Usuario("Wagner", "CDDTuc");//foi criado o método construct que permite passar os dados direto pela instância do objeto sem precisar chamar os setters
+$aluno->insert();
 
-//$aluno->insert();
+echo $aluno;
+*/
 
-//echo $aluno;
-
+/*
+//alteração de dados, primeiro faz uma consulta pra pegar os dados e depois chama o método passando os parâmetros que serão mudados
 $usuario = new Usuario();
 
 $usuario->loadById(8);
@@ -41,5 +55,12 @@ $usuario->loadById(8);
 $usuario->update("Arlindo", "negao20");
 
 echo $usuario;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(7);
+
+$usuario->delete();
 
 ?>
